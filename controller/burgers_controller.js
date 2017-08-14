232eb5e3c11f9b8d75ce0d6bb3b3f2d1c041fa02
Rosa Manriquez <rosamanriquez@Rosas-MacBook-Pro.local>
2017-08-13 22:10:02 -0700

    this didn't work out

---
+++ controller/burgers_controller.js
@@ -0,0 +1,47 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burgers = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+
+router.get('/', function(req, res) {
+  res.redirect('/burgers');
+}); 
+
+router.get("/burgers", function(req, res) {
+  burgers.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/burgers/insertOne", function(req, res) {
+  burgers.insertOne(["burger_name"],
+   "req.body.b_name", function(data) {
+    res.redirect("/burgers");
+  });
+});
+
+router.put("/burgers/updateOne/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burgers.updateOne({
+    "devoured": req.params.id }, 
+    condition, function(data) {
+    res.redirect("/burgers");
+  });
+});
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;

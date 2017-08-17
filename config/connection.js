a25b887971c4240c6d071d5945767a3c4efaf9cf
Rosa Manriquez <rosamanriquez@Rosas-MacBook-Pro.local>
2017-08-16 17:38:09 -0700

    push to git

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
+var connection; 
+if(process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",   
   password: "",
   database: "burger_db"
 });
+});
+
 
 // Connect to the database
 connection.connect(function(err) {
